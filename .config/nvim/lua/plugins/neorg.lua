dea4ed21ce42a85b0c5c581de0c9a66ba6318f05
MateiPostoaca <postoacamatei@gmail.com>
2024-06-28 21:27:32 +0300

    Setup notes

---
+++ .config/nvim/lua/plugins/neorg.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000,
+    config = true,
+  },
+  {
+    "nvim-neorg/neorg",
+    dependencies = { "luarocks.nvim" },
+    version = "*",
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {},
+          ["core.concealer"] = {},
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                Notes = "~/Documents/Notes",
+              },
+              default_workspace = "Notes",
+            },
+          },
+        },
+      }
+      vim.wo.foldlevel = 99
+      vim.wo.conceallevel = 2
+    end,
+  }
+}
